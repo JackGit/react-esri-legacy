2303d28056eaf2d80960c59cab962e7588ac4d35
ygjack <ygjack414@hotmail.com>
2018-07-02 17:37:49 +0800

    update

--- src/components/ModuleLoader.js
+++ src/components/ModuleLoader.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import EsriLoaderReact from 'esri-loader-react'
 import { noop } from '@/utils/func'
